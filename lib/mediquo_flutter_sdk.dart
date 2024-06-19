3b577155e0db7997ed77e361cc9eea29b1c38fc9
Juan Cava Platas <juan.cava.platas@gmail.com>
2024-06-19 22:17:51 +0200

    intercept back button and publish close javascript listener

--- lib/mediquo_flutter_sdk.dart
+++ lib/mediquo_flutter_sdk.dart
@@ -49,72 +49,87 @@ class _MediquoWidgetState extends State<MediquoWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('')
-      ),
-      body: Column(children: <Widget>[
-        Expanded(
-            child: Stack(
-              children: [
-                InAppWebView(
-                  key: webViewKey,
-                  initialUrlRequest: URLRequest(url: WebUri(url)),
-                  initialSettings: InAppWebViewSettings(
-                      transparentBackground: true,
-                      safeBrowsingEnabled: true,
-                      isFraudulentWebsiteWarningEnabled: true,
-                      mediaPlaybackRequiresUserGesture: false,
-                      allowsInlineMediaPlayback: true,
-                      iframeAllow: "camera; microphone",
-                      iframeAllowFullscreen: true,
-                      useOnDownloadStart: true
-                  ),
-                  onLoadStart: (controller, url) {
-                    if (url != null) {
-                      setState(() {
-                        this.url = url.toString();
-                        isSecure = urlIsSecure(url);
-                      });
-                    }
-                  },
-                  onLoadStop: (controller, url) async {
-                    if (url != null) {
-                      setState(() {
-                        this.url = url.toString();
-                      });
-                    }
-                  },
-                  onDownloadStartRequest: (InAppWebViewController controller, DownloadStartRequest request) {
-                    widget.onDownload(request.url.rawValue);
-                  },
-                  onPermissionRequest: (InAppWebViewController controller, PermissionRequest request) async {
-                    if (request.resources.contains(PermissionResourceType.MICROPHONE)) {
-                      await widget.onMicrophonePermission();
-                    }
+    return PopScope(
+      canPop: false,
+        onPopInvoked: (bool didPop) async {
+          if  (didPop) {
+            return;
+          }
+        },
+        child:  Scaffold(
+          appBar: AppBar(
+              title: const Text('')
+          ),
+          body: Column(children: <Widget>[
+            Expanded(
+                child: Stack(
+                  children: [
+                    InAppWebView(
+                      key: webViewKey,
+                      initialUrlRequest: URLRequest(url: WebUri(url)),
+                      initialSettings: InAppWebViewSettings(
+                        transparentBackground: true,
+                        safeBrowsingEnabled: true,
+                        isFraudulentWebsiteWarningEnabled: true,
+                        mediaPlaybackRequiresUserGesture: false,
+                        allowsInlineMediaPlayback: true,
+                        iframeAllow: "camera; microphone",
+                        iframeAllowFullscreen: true,
+                        useOnDownloadStart: true,
+                      ),
+                      onLoadStart: (controller, url) {
+                        if (url != null) {
+                          setState(() {
+                            this.url = url.toString();
+                            isSecure = urlIsSecure(url);
+                          });
+                        }
+                      },
+                      onLoadStop: (controller, url) async {
+                        if (url != null) {
+                          setState(() {
+                            this.url = url.toString();
+                          });
+                        }
+                      },
+                      onWebViewCreated: (controller) {
+                        controller.addJavaScriptHandler(
+                            handlerName: 'mediquo_flutter_sdk_close',
+                            callback: (args) {
+                              Navigator.pop(context);
+                            });
+                      },
+                      onDownloadStartRequest: (InAppWebViewController controller, DownloadStartRequest request) {
+                        widget.onDownload(request.url.rawValue);
+                        },
+                      onPermissionRequest: (InAppWebViewController controller, PermissionRequest request) async {
+                        if (request.resources.contains(PermissionResourceType.MICROPHONE)) {
+                          await widget.onMicrophonePermission();
+                        }
 
-                    if (request.resources.contains(PermissionResourceType.CAMERA)) {
-                      await widget.onCameraPermission();
-                    }
+                        if (request.resources.contains(PermissionResourceType.CAMERA)) {
+                          await widget.onCameraPermission();
+                        }
 
-                    return PermissionResponse(
-                        resources: request.resources,
-                        action: PermissionResponseAction.GRANT
-                    );
-                  },
-                  shouldOverrideUrlLoading: (InAppWebViewController controller, NavigationAction navigationAction) async {
-                    final uri = navigationAction.request.url!;
-                    if (uri.toString().contains('mediquo.com')) {
-                      return NavigationActionPolicy.ALLOW;
-                    }
+                        return PermissionResponse(
+                            resources: request.resources,
+                            action: PermissionResponseAction.GRANT
+                        );
+                        },
+                      shouldOverrideUrlLoading: (InAppWebViewController controller, NavigationAction navigationAction) async {
+                        final uri = navigationAction.request.url!;
+                        if (uri.toString().contains('mediquo.com')) {
+                          return NavigationActionPolicy.ALLOW;
+                        }
 
-                    widget.onLoadUrl(uri.toString());
-                    return NavigationActionPolicy.CANCEL;
-                  },
-                ),
-              ],
-            )),
-      ]),
+                        widget.onLoadUrl(uri.toString());
+                        return NavigationActionPolicy.CANCEL;
+                        },
+                    ),
+                  ],
+                )),
+          ]),
+        )
     );
   }
 
