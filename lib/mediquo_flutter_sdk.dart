de289b860695c3075028b2b71fa9664e85ea5bae
Juan Cava Platas <juan.cava.platas@gmail.com>
2024-06-26 12:49:46 +0200

    add transparent background color

--- lib/mediquo_flutter_sdk.dart
+++ lib/mediquo_flutter_sdk.dart
@@ -57,86 +57,88 @@ class _MediquoWidgetState extends State<MediquoWidget> {
           }
           return;
         },*/
-        child:  Scaffold(
-          /*appBar: AppBar(
-              title: const Text('')
-          ),*/
-          body: Column(children: <Widget>[
-            Expanded(
-                child: Stack(
-                  children: [
-                    InAppWebView(
-                      key: webViewKey,
-                      initialUrlRequest: URLRequest(url: WebUri(url)),
-                      initialSettings: InAppWebViewSettings(
-                        transparentBackground: true,
-                        safeBrowsingEnabled: true,
-                        isFraudulentWebsiteWarningEnabled: true,
-                        mediaPlaybackRequiresUserGesture: false,
-                        allowsInlineMediaPlayback: true,
-                        iframeAllow: "camera; microphone",
-                        iframeAllowFullscreen: true,
-                        useOnDownloadStart: true,
-                        cacheEnabled: false
-                      ),
-                      onLoadStart: (controller, url) {
-                        if (url != null) {
-                          setState(() {
-                            this.url = url.toString();
-                            isSecure = urlIsSecure(url);
-                          });
-                        }
-                      },
-                      onLoadStop: (controller, url) async {
-                        if (url != null) {
-                          setState(() {
-                            this.url = url.toString();
-                          });
-                        }
-                      },
-                      onWebViewCreated: (controller) {
-                        controller.addJavaScriptHandler(
-                            handlerName: 'mediquo_flutter_sdk_close',
-                            callback: (args) {
-                              Navigator.pop(context);
-                            });
-                      },
-                      onDownloadStartRequest: (InAppWebViewController controller, DownloadStartRequest request) {
-                        widget.onDownload(request.url.rawValue);
-                        },
-                      onPermissionRequest: (InAppWebViewController controller, PermissionRequest request) async {
-                        if (request.resources.contains(PermissionResourceType.MICROPHONE)) {
-                          await widget.onMicrophonePermission();
-                        }
+        child: new Container(
+          color: Colors.transparent,
+          child: new SafeArea(
+              child: Scaffold(
+                body: Column(children: <Widget>[
+                  Expanded(
+                      child: Stack(
+                        children: [
+                          InAppWebView(
+                            key: webViewKey,
+                            initialUrlRequest: URLRequest(url: WebUri(url)),
+                            initialSettings: InAppWebViewSettings(
+                                transparentBackground: true,
+                                safeBrowsingEnabled: true,
+                                isFraudulentWebsiteWarningEnabled: true,
+                                mediaPlaybackRequiresUserGesture: false,
+                                allowsInlineMediaPlayback: true,
+                                iframeAllow: "camera; microphone",
+                                iframeAllowFullscreen: true,
+                                useOnDownloadStart: true,
+                                cacheEnabled: false
+                            ),
+                            onLoadStart: (controller, url) {
+                              if (url != null) {
+                                setState(() {
+                                  this.url = url.toString();
+                                  isSecure = urlIsSecure(url);
+                                });
+                              }
+                            },
+                            onLoadStop: (controller, url) async {
+                              if (url != null) {
+                                setState(() {
+                                  this.url = url.toString();
+                                });
+                              }
+                            },
+                            onWebViewCreated: (controller) {
+                              controller.addJavaScriptHandler(
+                                  handlerName: 'mediquo_flutter_sdk_close',
+                                  callback: (args) {
+                                    Navigator.pop(context);
+                                  });
+                            },
+                            onDownloadStartRequest: (InAppWebViewController controller, DownloadStartRequest request) {
+                              widget.onDownload(request.url.rawValue);
+                            },
+                            onPermissionRequest: (InAppWebViewController controller, PermissionRequest request) async {
+                              if (request.resources.contains(PermissionResourceType.MICROPHONE)) {
+                                await widget.onMicrophonePermission();
+                              }
 
-                        if (request.resources.contains(PermissionResourceType.CAMERA)) {
-                          await widget.onCameraPermission();
-                        }
+                              if (request.resources.contains(PermissionResourceType.CAMERA)) {
+                                await widget.onCameraPermission();
+                              }
 
-                        return PermissionResponse(
-                            resources: request.resources,
-                            action: PermissionResponseAction.GRANT
-                        );
-                        },
-                      shouldOverrideUrlLoading: (InAppWebViewController controller, NavigationAction navigationAction) async {
-                        final uri = navigationAction.request.url!;
-                        if (uri.toString().contains('mediquo.com')) {
+                              return PermissionResponse(
+                                  resources: request.resources,
+                                  action: PermissionResponseAction.GRANT
+                              );
+                            },
+                            shouldOverrideUrlLoading: (InAppWebViewController controller, NavigationAction navigationAction) async {
+                              final uri = navigationAction.request.url!;
+                              if (uri.toString().contains('mediquo.com')) {
 
-                          if (uri.toString().contains('privacy') | uri.toString().contains('terms')) {
-                            widget.onLoadUrl(uri.toString());
-                            return NavigationActionPolicy.CANCEL;
-                          }
-                          
-                          return NavigationActionPolicy.ALLOW;
-                        }
+                                if (uri.toString().contains('privacy') | uri.toString().contains('terms')) {
+                                  widget.onLoadUrl(uri.toString());
+                                  return NavigationActionPolicy.CANCEL;
+                                }
 
-                        widget.onLoadUrl(uri.toString());
-                        return NavigationActionPolicy.CANCEL;
-                        },
-                    ),
-                  ],
-                )),
-          ]),
+                                return NavigationActionPolicy.ALLOW;
+                              }
+
+                              widget.onLoadUrl(uri.toString());
+                              return NavigationActionPolicy.CANCEL;
+                            },
+                          ),
+                        ],
+                      )),
+                ]),
+              )
+          ),
         )
     );
   }
